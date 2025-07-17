3886efdf7be71ac54030ea85c24c146433b1c70f
Avi <santosh.prasadindia71@gmail.com>
2025-07-17 14:39:32 +0530

    New loops programs

---
+++ DoWhileLoop.java
@@ -0,0 +1,13 @@
+
+// Print 1 to 100 using a do while loop
+import java.util.Scanner;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1; // Initialize counter
+        do {
+            System.out.println(i); // Print current number
+            i++; // Increment counter
+        } while (i <= 100); // Condition to check
+    }
+}
---
+++ WhileLoop.java
@@ -0,0 +1,13 @@
+
+// Print 1 to 100 using a while loop
+import java.util.Scanner;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1; // Initialize counter
+        while (i <= 100) { // Condition to check
+            System.out.println(i); // Print current number
+            i++; // Increment counter
+        }
+    }
+}
